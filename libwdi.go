5ce9d6994809d315531b38219eda4ac1c7d75cbd
Matthew Camp <mattscamp@gmail.com>
2022-09-03 12:16:18 -0600

    chore: update libwdi

--- libwdi.go
+++ libwdi.go
@@ -3,107 +3,82 @@
 
 package zerousb
 
-/*
-	#include "./libwdi/libwdi/libwdi.h"
-
-	struct wdi_device_info {
-		struct wdi_device_info *next;
-		unsigned short vid;
-		unsigned short pid;
-		BOOL is_composite;
-		unsigned char mi;
-		char* desc;
-		char* driver;
-		char* device_id;
-		char* hardware_id;
-		char* compatible_id;
-		char* upper_filter;
-		UINT64 driver_version;
-	};
-
-	struct wdi_options_create_list {
-		BOOL list_all;
-		BOOL list_hubs;
-		BOOL trim_whitespaces;
-	};
-
-	struct wdi_options_prepare_driver {
-		int driver_type;
-		char* vendor_name;
-		char* device_guid;
-		BOOL disable_cat;
-		BOOL disable_signing;
-		char* cert_subject;
-		BOOL use_wcid_driver;
-		BOOL external_inf;
-	};
-
-	struct wdi_options_install_driver {
-		HWND hWnd;
-		BOOL install_filter_driver;
-		UINT32 pending_install_timeout;
-	};
-*/
+// #include "./libwdi/libwdi/libwdi.h"
 import "C"
 
 import (
 	"fmt"
+	"unsafe"
 )
 
 type wdiDeviceInfo struct {
-	c C.wdi_device_info
+	c C.struct_wdi_device_info
 }
 type wdiOptionsCreateList struct {
-	c C.wdi_options_create_list
+	c C.struct_wdi_options_create_list
 }
 type wdiOptionsPrepareDriver struct {
-	c C.wdi_options_prepare_driver
+	c C.struct_wdi_options_prepare_driver
 }
 type wdiOptionsInstallDriver struct {
-	c C.wdi_options_install_driver
+	c C.struct_wdi_options_install_driver
 }
 
-const infName =   "usb_device.inf"
-const defaultDir = "usb_driver"
+func installWindowsDrivers(vid uint, pid uint, desc string) {
+	infName := C.CString("usb_device.inf")
+	defaultDir := C.CString("usb_driver")
+	description := C.CString(desc)
 
-func installWindowsDrivers(vid uint, pid uint) {
-	dev := &wdiDeviceInfo{
-		c: C.wdi_device_info
+	defer C.free(unsafe.Pointer(infName))
+	defer C.free(unsafe.Pointer(defaultDir))
+	defer C.free(unsafe.Pointer(description))
+
+	dev := &C.struct_wdi_device_info{
+		vid:  (C.ushort)(vid),
+		pid:  (C.ushort)(pid),
+		desc: description,
 	}
-	opd := wdiOptionsPrepareDriver{
-		c: C.wdi_options_prepare_driver{
-			driver_type: C.WDI_WINUSB,
-		}
+	defer C.free(unsafe.Pointer(dev))
+	ldev := &C.struct_wdi_device_info{}
+	defer C.free(unsafe.Pointer(ldev))
+	opd := &C.struct_wdi_options_prepare_driver{
+		driver_type: C.WDI_WINUSB,
 	}
-	r := C.wdi_prepare_driver(&dev, defaultDir, infName, &opd.c)
+	defer C.free(unsafe.Pointer(opd))
+	ocl := &C.struct_wdi_options_create_list{}
+	defer C.free(unsafe.Pointer(ocl))
+	oid := &C.struct_wdi_options_install_driver{}
+	defer C.free(unsafe.Pointer(oid))
+
+	r := C.wdi_prepare_driver(dev, defaultDir, infName, opd)
 
 	fmt.Printf("%v \n", C.wdi_strerror(r))
 
-	if (r != C.WDI_SUCCESS) {
+	if r != C.WDI_SUCCESS {
 		return
 	}
 	fmt.Printf("%v \n", r)
 
 	// Try to match against a plugged device to avoid device manager prompts
 	foundDevice := false
-	if (C.wdi_create_list(&ldev, &ocl) == C.WDI_SUCCESS) {
-		r = C.WDI_SUCCESS;
-		for (ldev != NULL) && (r == WDI_SUCCESS) {
-			ldev = ldev->next
-			if (ldev.vid == dev.vid) && (ldev.pid == dev.pid) && (ldev.mi == dev.mi) &&(ldev.is_composite == dev.is_composite) {
+	if C.wdi_create_list(&ldev, ocl) == C.WDI_SUCCESS {
+		r = C.WDI_SUCCESS
+		for ldev != nil && r == C.WDI_SUCCESS {
+			ldev = ldev.next
+			if (ldev.vid == dev.vid) && (ldev.pid == dev.pid) && (ldev.mi == dev.mi) && (ldev.is_composite == dev.is_composite) {
 				dev.hardware_id = ldev.hardware_id
 				dev.device_id = ldev.device_id
 				foundDevice = true
-				fmt.Printf("%v: ", dev.hardware_id);
-				r = C.wdi_install_driver(&dev, ext_dir, inf_name, &oid);
-				fmt.Printf("%v\n", C.wdi_strerror(r));
+				fmt.Printf("%v: ", dev.hardware_id)
+				r = C.wdi_install_driver(dev, defaultDir, infName, oid)
+				fmt.Printf("%v\n", C.wdi_strerror(r))
 			}
 		}
 	}
 
 	// No plugged USB device matches this one -> install driver
-	if (!foundDevice) {
-		r = C.wdi_install_driver(&dev, ext_dir, inf_name, &oid);
-		fmt.Prrintf("%v\n", C.wdi_strerror(r));
+	if !foundDevice {
+		r = C.wdi_install_driver(dev, defaultDir, infName, oid)
+		fmt.Printf("%v\n", C.wdi_strerror(r))
 	}
 }
